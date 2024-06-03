2fd3d9932662e35bb203597dd18028f091e65509
ana117 <63540951+ana117@users.noreply.github.com>
2024-06-03 20:01:25 +0700

    feat: add approve/reject

--- database.py
+++ database.py
@@ -26,18 +26,26 @@ class UUIDEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
     
 
-def post(path: str, data: dict):
-    url = f"{DB_SERVICE_URL}/payment/{path}"
+def post(path: str, data: dict = {}):
+    url = f"{DB_SERVICE_URL}/{path}"
     try:
         response = requests.post(url, json=data)
         return response.json()
     except Exception as e:
         raise ValueError(str(e))
 
-def get(path: str, params: dict):
-    url = f"{DB_SERVICE_URL}/payment/{path}"
+def get(path: str, params: dict = {}):
+    url = f"{DB_SERVICE_URL}/{path}"
     try:
         response = requests.get(url, params=params)
+        return response.json()
+    except Exception as e:
+        raise ValueError(str(e))
+
+def put(path: str, data: dict = {}):
+    url = f"{DB_SERVICE_URL}/{path}"
+    try:
+        response = requests.put(url, json=data)
         return response.json()
     except Exception as e:
         raise ValueError(str(e))
--- payment/router.py
+++ payment/router.py
@@ -1,25 +1,31 @@
+import json
 from fastapi import APIRouter, Depends, Form, HTTPException, Response, UploadFile
 from supabase import Client, StorageException
 
-from database import get_supabase, post, get
+from database import get_supabase, post, get, put
 from payment import schema
 
 router = APIRouter()
 
 
-@router.get("/transaction/", response_model=schema.Transaction)
+@router.get("/transaction/{transaction_id}", response_model=schema.Transaction)
 async def get_transaction(transaction_id: str):
     try:
-        return get("transaction", {"transaction_id": transaction_id})
+        return get("payment/transaction", {"transaction_id": transaction_id})
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
     
 
-@router.get("/transaction/detail/", response_model=schema.TransactionDetail)
+@router.get("/transaction/detail/{transaction_id}", response_model=schema.TransactionDetail)
 async def get_transaction_detail(transaction_id: str):
     try:
-        transaction = get("transaction", {"transaction_id": transaction_id})
-        tryout = get("tryout/tryout", {"tryout_id": str(transaction["tryout_id"])})
+        transaction = get(f"payment/transaction/{transaction_id}")
+
+        tryout_id = str(transaction["tryout_id"])
+        tryout = get(f"tryout/{tryout_id}")
+
+        if transaction is None or tryout is None:
+            raise HTTPException(status_code=404, detail="Transaction or tryout not found")
 
         transaction["transaction_id"] = transaction["id"]
         transaction["tryout_name"] = tryout["title"]
@@ -34,23 +40,23 @@ async def get_transaction_detail(transaction_id: str):
 @router.get("/transactions/", response_model=list[schema.Transaction])
 async def get_transactions(skip: int = 0, limit: int = 100):
     try:
-        return get("transactions", {"skip": skip, "limit": limit})
+        return get("payment/transactions", {"skip": skip, "limit": limit})
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/transactions/user/", response_model=list[schema.Transaction])
+@router.get("/transactions/user/{user_id}", response_model=list[schema.Transaction])
 async def get_transactions_by_user(user_id: str, skip: int = 0, limit: int = 100):
     try:
-        return get("transactions/user", {"user_id": user_id, "skip": skip, "limit": limit})
+        return get(f"payment/transactions/user/{user_id}", {"skip": skip, "limit": limit})
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/transactions/tryout/", response_model=list[schema.Transaction])
+@router.get("/transactions/tryout/{tryout_id}", response_model=list[schema.Transaction])
 async def get_transactions_by_tryout(tryout_id: str, skip: int = 0, limit: int = 100):
     try:
-        return get("transactions/tryout", {"tryout_id": tryout_id, "skip": skip, "limit": limit})
+        return get(f"payment/transactions/tryout/{tryout_id}", {"skip": skip, "limit": limit})
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
@@ -61,41 +67,63 @@ async def create_transaction(transaction: schema.TransactionCreate):
         data = transaction.model_dump()
         data["tryout_id"] = str(data["tryout_id"])
         data["user_id"] = str(data["user_id"])
-        return post("transaction", data)
+        return post("payment/transaction", data)
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.put("/transaction/", response_model=schema.Transaction)
-async def update_transaction(transaction_id: str, updated_transaction: schema.TransactionCreate):
+@router.post("/approve/{transaction_id}", response_model=schema.Transaction)
+async def approve_transaction(transaction_id: str):
     try:
-        return post(f"transaction/{transaction_id}", updated_transaction.model_dump())
+        transaction = get(f"payment/transaction/{transaction_id}")
+        if transaction is None:
+            raise HTTPException(status_code=404, detail="Transaction not found")
+        
+        transaction["status"] = "approved"
+        return put(f"payment/transaction/{transaction_id}", transaction)
+    except ValueError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.post("/reject/{transaction_id}", response_model=schema.Transaction)
+async def reject_transaction(transaction_id: str):
+    try:
+        transaction = get(f"payment/transaction/{transaction_id}")
+        if transaction is None:
+            raise HTTPException(status_code=404, detail="Transaction not found")
+        
+        transaction["status"] = "rejected"
+        return put(f"payment/transaction/{transaction_id}", transaction)
     except ValueError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/proof/", responses={200: {"content": {"image/png": {}}}})
-async def get_proof_of_payment(tid: str, supabase: Client = Depends(get_supabase)):
+@router.get("/proof/{transaction_id}", responses={200: {"content": {"image/png": {}}}})
+async def get_proof_of_payment(transaction_id: str, supabase: Client = Depends(get_supabase)):
     try:
-        response: bytes = supabase.storage.from_("proofs").download(path=tid)
+        response: bytes = supabase.storage.from_("proofs").download(path=transaction_id)
         return Response(content=response, media_type="image/png")
     except StorageException:
         raise HTTPException(status_code=404, detail="Proof of payment not found")
 
 
-@router.post("/proof/", response_model=str)
-async def upload_proof_of_payment(file: UploadFile, tid: str = Form(...), supabase: Client = Depends(get_supabase)):
+@router.post("/proof/{transaction_id}", response_model=str)
+async def upload_proof_of_payment(file: UploadFile, transaction_id: str, supabase: Client = Depends(get_supabase)):
     if not file.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image file")
     
-    transaction = await get("transaction", {"transaction_id": tid})
+    transaction = get(f"payment/transaction/{transaction_id}")
     if transaction is None:
         raise HTTPException(status_code=404, detail="Transaction not found")
 
     file_bytes = await file.read()
-    upload_response = supabase.storage.from_("proofs").upload(file=file_bytes, path=tid, file_options={"content-type": file.content_type})
+    try:
+        upload_response = supabase.storage.from_("proofs").upload(file=file_bytes, path=transaction_id, file_options={"content-type": file.content_type})
 
-    if upload_response.status_code != 200:
-        raise HTTPException(status_code=400, detail="Failed to upload proof of payment")
+        if upload_response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Failed to upload proof of payment")
+    except StorageException as e:
+        error_dict = json.loads(str(e).replace("'", "\""))
+        raise HTTPException(status_code=error_dict["statusCode"], detail=error_dict["message"])
     
-    return tid
+    return transaction_id
